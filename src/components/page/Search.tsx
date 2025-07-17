47f9a508259d1e5cdb9f254acff9c270895df50c
lade-devs <olumayokunolayinka@gmail.com>
2025-07-17 13:29:50 +0100

    [update]

--- src/components/page/Search.tsx
+++ src/components/page/Search.tsx
@@ -41,7 +41,7 @@ export default function SearchPage({ query }: { query: string }) {
     }, [search, GetListing]);
 
     useEffect(() => {
-        if(query){
+        if (query) {
             setSearch(strReplace(query, '-', ' '));
         }
     }, [query]);
